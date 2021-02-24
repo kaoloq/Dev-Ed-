7e549a83c79887e7f534165cc2b7a226f165ec5c
Кушкоева Анастасия <31595247+kaoloq@users.noreply.github.com>
2021-02-25 00:33:31 +0300

    Add files via upload

---
+++ variables/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write A:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write B:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int division = a / b;
+            int mod = a % b;
+
+            Console.WriteLine($"Division A by B = {division}");
+            Console.WriteLine($"Mod = {mod}");
+        }
+    }
+}
