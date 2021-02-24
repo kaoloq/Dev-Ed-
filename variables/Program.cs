0cdf4491559ae34d09e75a1ee4d0137112a0e77b
Кушкоева Анастасия <31595247+kaoloq@users.noreply.github.com>
2021-02-25 00:28:38 +0300

    Add files via upload

---
+++ variables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write A: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Write B: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double result = (5 * a + b * b) / (b - a);
+
+            Console.WriteLine($"result = {result}");
+        }
+    }
+}
