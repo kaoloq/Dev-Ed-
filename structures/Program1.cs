0d8f5779872c1687353bc0ee9956f0523eba0b92
Кушкоева Анастасия <31595247+kaoloq@users.noreply.github.com>
2021-03-01 02:11:47 +0300

    Add files via upload

---
+++ structures/Program1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+ //Пользователь вводит 2 числа (A и B). Если A>B, подсчитать A+B, если A=B, подсчитать A*B, если A<B, подсчитать A-B.
+
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine($"A + B = {a + b}");
+            }
+            else if (a == b) 
+            {
+                Console.WriteLine($"A * B = {a * b}");
+            }
+            else
+            {
+                Console.WriteLine($"A - B = {a - b}");
+            }
+
+        }
+    }
+}
